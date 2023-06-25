84284d6c6fd56d7bf2180d7d7a565c0f756dc715
Mohammad Arsalan <iservicez.dev@gmail.com>
2023-06-26 02:25:55 +0500

    setup project -Add app icon -set min ios target 13.0

---
+++ CaptureCraze/CaptureCraze/Views/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  CaptureCraze
+//
+//  Created by Mohammad Arsalan on 26/06/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Capture Craze")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
